ad2a32c3d719c5766bfab3cb398af358f6eba4ab
Chris Sawczuk <gummybassist@gmail.com>
2015-12-06 20:05:38 +0000

    Second and third test passing, now modelling an infinite map

--- test/SantaMapTest.java
+++ test/SantaMapTest.java
@@ -13,14 +13,66 @@ public class SantaMapTest {
         assertThat(santaMap.uniquePlaces(), is(2));
     }
 
-    public class SantaMap {
+    @Test
+    public void whenSantaNavigatesEastAndThenWest_santaHasOnlyVisitedTwoUniquePlaces()
+    {
+        SantaMap santaMap = new SantaMap();
+        santaMap.moveEast();
+        santaMap.moveWest();
+        assertThat(santaMap.uniquePlaces(), is(2));
+    }
+    @Test
+    public void whenSantaNavigatesEastAndThenWestTwice_santaHasVisitedThreeUniquePlaces()
+    {
+        SantaMap santaMap = new SantaMap();
+        santaMap.moveEast();
+        santaMap.moveWest();
+        santaMap.moveWest();
+        assertThat(santaMap.uniquePlaces(), is(3));
+    }
+
+    public static class SantaMap {
+        private static class Location
+        {
+            private Location north;
+            private Location south;
+            private Location west;
+            private Location east;
+
+            public static void fromLocationToWest(Location westernLocation) {
+                Location location = new Location();
+                westernLocation.east = location;
+                location.west = westernLocation;
+            }
+
+            public static void fromLocationToEast(Location easternLocation) {
+                Location location = new Location();
+                easternLocation.west = location;
+                location.east = easternLocation;
+            }
+        }
+
+        private Location currentLocation = new Location();
+
         private int uniquePlaces = 1;
         public void moveEast() {
-            uniquePlaces++;
+            if(currentLocation.east == null) {
+                Location.fromLocationToWest(currentLocation);
+                uniquePlaces++;
+            }
+            currentLocation = currentLocation.east;
         }
 
         public int uniquePlaces() {
             return uniquePlaces;
+        }
+
+        public void moveWest() {
+            if(currentLocation.west == null) {
+                Location.fromLocationToEast(currentLocation);
+                uniquePlaces++;
+            }
+            currentLocation = currentLocation.west;
         }
     }
 }
